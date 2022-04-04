629873b80a8ebb8826ff7aadd729b9b6f39d5f32
Arun Gupta <arung2207@gmail.com>
2022-04-04 16:47:53 +0530

    Hello world from Arun

---
+++ FirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arun Gupta: Hello World!");
+        }
+    }
+}
