e14308ecc4104e7a7ee486c7ffb1e3cb5494a77d
mohan455 <102985148+mohan455@users.noreply.github.com>
2022-04-04 22:16:00 +0530

    Update Program.cs

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,7 +7,7 @@ namespace FirstApp
         static void Main(string[] args)
         {
             Console.WriteLine("Arun G: Hello World!");
-            Console.WriteLine("Atharva Pawar: Hello World!");
+            Console.WriteLine("Mohan K: Hello World!");
         }
     }
 }
