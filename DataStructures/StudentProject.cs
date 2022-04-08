784a573cd5205687dae87541e9e7f9cb8ed53a44
Arun Gupta <arung2207@gmail.com>
2022-04-08 19:58:27 +0530

    Skeleton StudentProject

---
+++ DataStructures/StudentProject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class StudentProject
+    {
+        //Read the text file and display the contents of the file
+        //Create an array out of the values
+        //Sort the array
+        //Display the array
+        //Enable searching in the list if cities
+
+        //Mumbai
+        //Delhi
+        //Bangalore
+        //Hyderabad
+        //Ahmedabad
+        //Chennai
+        //Kolkata
+        //Surat
+        //Pune
+        //Jaipur
+        //Lucknow
+        //Kanpur
+        //Nagpur
+        //Indore
+        //Thane
+    }
+}
