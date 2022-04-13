5bd5580bfae204572b8a5d0a41a1d51023dc23a1
Arun Gupta <arung2207@gmail.com>
2022-04-13 22:11:59 +0530

    Singleton design pattern

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            Singleton fromTeachear = Singleton.GetInstance;
+            fromTeachear.PrintDetails("From Teacher");
+            
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+
+        public Singleton() //private constructor ensures that nobody can create an instance from outside
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+
+        public static Singleton GetInstance //get reference to the instance of class, if its not create it gets created on first access because of private constructor
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
