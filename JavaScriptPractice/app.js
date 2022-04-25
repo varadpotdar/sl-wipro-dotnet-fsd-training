e4d14e70ae9956f68237f1179335533ae97dcad1
Arun Gupta <arung2207@gmail.com>
2022-04-25 22:23:15 +0530

    With list of pratice porgrams

--- JavaScriptPractice/app.js
+++ JavaScriptPractice/app.js
@@ -1,3 +1,35 @@
 'use strict';
 
 console.log('Hello world');
+
+//--------------------------------------------------------
+//Program 1: Print all the multiplication tables with numbers from 1 to 10
+
+//6 * 1 = 6
+//6 * 2 = 12
+
+//    .
+//.
+//.
+//.
+//.
+//6 * 10 = 60
+//--------------------------------------------------------
+
+//    program 2: Find the maximum number in an array of numbers
+
+//---------------------------------
+
+//    program 3: Calculate the sum of digits of a positive integer number
+
+//----------------------------
+
+//    program 4: Print the first 100 prime numbers
+
+//--------------------------------------
+
+//program 5: Given a string, verify if it is a palindrome.
+// No lemon, no melon!
+// Borrow or rob ?
+
+//--------------------------------------
