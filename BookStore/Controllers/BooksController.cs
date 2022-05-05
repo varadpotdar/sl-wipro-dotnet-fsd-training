4f725d91c06cb25710ca4cad361d466fc20df489
Arun Gupta <arung2207@gmail.com>
2022-05-05 20:10:01 +0530

    Books Controller

---
+++ BookStore/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            Book book = new Book()
+            {
+                Id = 1,
+                Title = "Learning ASP.NET Core 2.0",
+                Genre = "Programming & Software Development",
+                Price = 45,
+                PublishDate = new System.DateTime(2012, 04, 23),
+                Authors = new List<string> { "Jason De Oliveira", "Michel Bruchet" }
+            };
+
+            return View(book);
+        }
+    }
+}
---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
