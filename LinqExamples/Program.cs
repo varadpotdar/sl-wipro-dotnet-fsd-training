1cf878911d6030de53842e6346bd047e9458c90a
Arun Gupta <arung2207@gmail.com>
2022-05-12 19:40:54 +0530

    Linq example

--- LinqExamples/Program.cs
+++ LinqExamples/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             //Demo1();
-            Demo2();
-
+            //Demo2();
+            Demo3();
         }
 
         private static void Demo1()
@@ -50,6 +50,28 @@
             Student[] students = StudentExtension.Where(studentArray, delegate (Student std) {
                 return std.Age > 12 && std.Age < 20;
             });
+        }
+
+        private static void Demo3()
+        {
+            Student[] studentArray = {
+                    new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                    new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
+                    new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                    new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                    new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 } ,
+                    new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 } ,
+                    new Student() { StudentID = 7, StudentName = "Rob",Age = 19  } ,
+                };
+
+            // Use LINQ to find teenager students
+            Student[] teenAgerStudents = studentArray.Where(s => s.Age > 12 && s.Age < 20).ToArray();
+
+            // Use LINQ to find first student whose name is Bill 
+            Student bill = studentArray.Where(s => s.StudentName == "Bill").FirstOrDefault();
+
+            // Use LINQ to find student whose StudentID is 5
+            Student student5 = studentArray.Where(s => s.StudentID == 5).FirstOrDefault();
         }
     }
 }
