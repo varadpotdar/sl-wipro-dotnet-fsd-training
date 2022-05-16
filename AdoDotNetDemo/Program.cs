7405b08e1083ebf5813090168c43230d29614704
Arun Gupta <arung2207@gmail.com>
2022-05-16 21:38:17 +0530

    Basic Crud using ado.net

--- AdoDotNetDemo/Program.cs
+++ AdoDotNetDemo/Program.cs
@@ -4,26 +4,50 @@ namespace AdoNetConsoleApplication
 {
     class Program
     {
+        string connectionString = $"data source=(local);database=student2; integrated security=SSPI";
+        
+        private SqlConnection GetSqlConnection()
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            return sqlConnection;
+        }
+
+        private int ExecuteNonQuery(string query)
+        {
+            using(SqlConnection sqlConnection = GetSqlConnection())
+            {
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        private SqlDataReader GetReader(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query, GetSqlConnection());
+            return cmd.ExecuteReader();
+        }
+
         static void Main(string[] args)
         {
             new Program().CreateTable();
+            Console.ReadLine();
+
+            new Program().InsertData();
+            Console.ReadLine();
+
+            new Program().ReadData();
+            Console.ReadLine();
+
+            new Program().DeleteData();
+            Console.ReadLine();
         }
+        
         public void CreateTable()
         {
-            SqlConnection con = null;
             try
             {
-                // Creating Connection  
-                con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
-                
-                // writing sql query  
-                SqlCommand cm = new SqlCommand("create table student(id int not null, name varchar(100), email varchar(50), join_date date)", con);  
-                
-                // Opening Connection  
-                con.Open();
-                
-                // Executing the SQL query  
-                cm.ExecuteNonQuery();
+                ExecuteNonQuery("create table student(id int not null, name varchar(100), email varchar(50), join_date date)");
                 
                 // Displaying a message  
                 Console.WriteLine("Table created Successfully");
@@ -32,10 +56,54 @@ namespace AdoNetConsoleApplication
             {
                 Console.WriteLine("OOPs, something went wrong." + e);
             }
-            // Closing the connection  
-            finally
+        }
+
+        public void InsertData()
+        {
+            try
             {
-                con.Close();
+                ExecuteNonQuery("insert into student (id, name, email, join_date)values('101', 'Ronald Trump', 'ronald@example.com', '1/12/2017')");
+
+                // Displaying a message  
+                Console.WriteLine("Record Inserted Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+        }
+
+        public void ReadData()
+        {
+            try
+            {
+
+                // Executing the SQL query  
+                SqlDataReader sdr = GetReader("Select * from student");
+                
+                // Iterating Data  
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["id"] + " " + sdr["name"] + " " + sdr["email"]); // Displaying Record  
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong.\n" + e);
+            }
+        }
+
+        public void DeleteData()
+        {
+            try
+            {
+
+                ExecuteNonQuery("delete from student where id = '101'");
+                Console.WriteLine("Record Deleted Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong.\n" + e);
             }
         }
     }
