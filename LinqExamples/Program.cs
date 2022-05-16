dcb2323e9ff4d3abb3acedd317aa2cbf18131229
Arun Gupta <arung2207@gmail.com>
2022-05-16 19:19:00 +0530

    Linq programs

--- LinqExamples/Program.cs
+++ LinqExamples/Program.cs
@@ -10,8 +10,33 @@
             //Demo3();
             //Demo4();
             //Demo5();
-            Demo6();
+            //Demo6();
             //Demo7();
+
+            Demo8();
+        }
+
+        private static void Demo8()
+        {
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Abram" , Age = 21 }
+            };
+
+            var groupedResult = from s in studentList
+                                group s by s.Age;
+
+            //iterate each group        
+            foreach (var ageGroup in groupedResult)
+            {
+                Console.WriteLine("Age Group: {0}", ageGroup.Key); //Each group has a key 
+
+                foreach (Student s in ageGroup) // Each group has inner collection
+                    Console.WriteLine("Student Name: {0}", s.StudentName);
+            }
         }
 
         private static void Demo1()
