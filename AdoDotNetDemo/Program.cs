0cd6d8ec47ea0de679025fcf4a11f0517866ff43
Arun Gupta <arung2207@gmail.com>
2022-05-16 19:29:34 +0530

    Ado .net intial project

---
+++ AdoDotNetDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace AdoNetConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+                
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("create table student(id int not null, name varchar(100), email varchar(50), join_date date)", con);  
+                
+                // Opening Connection  
+                con.Open();
+                
+                // Executing the SQL query  
+                cm.ExecuteNonQuery();
+                
+                // Displaying a message  
+                Console.WriteLine("Table created Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
