f191f8016ef15479864ec16a2913673d4206e319
Arun Gupta <arung2207@gmail.com>
2022-05-19 19:47:22 +0530

    web api demo

---
+++ WebApiDemo/Data/WebApiDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Data
+{
+    public class WebApiDemoContext : DbContext
+    {
+        public WebApiDemoContext (DbContextOptions<WebApiDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiDemo.Models.TodoItem>? TodoItem { get; set; }
+    }
+}
