f72dd694a14d7f33646bb420b07aa490a6e113f3
Arun Gupta <arung2207@gmail.com>
2022-05-24 20:34:09 +0530

    Basic tests

--- SeleniumTests/UITests.cs
+++ SeleniumTests/UITests.cs
@@ -21,10 +21,40 @@ namespace SeleniumTests
         public void GotoUrl()
         {
             driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+
+            var en = driver.FindElement(By.Id("js-link-box-en"));
+            en.Click();
+        }
+
+
+        [Test]
+        public void GoogleSearchExample()
+        {
+            Console.Write("test case started ");
+
+            // navigate to URL  
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Thread.Sleep(2000);
+
+            // identify the Google search text box  
+            IWebElement searchTextBox = driver.FindElement(By.Name("q"));
+
+            //enter the value in the google search text box  
+            searchTextBox.SendKeys("covid cases");
+            Thread.Sleep(2000);
+
+            //identify the google search button  
+            IWebElement btnIFeelLucky = driver.FindElement(By.Name("btnI"));
+
+            // click on the Google search button  
+            btnIFeelLucky.Click();
+            Thread.Sleep(3000);
+
+            Console.Write("test case ended ");
         }
 
         [TearDown]
-        public void close_Browser()
+        public void CloseBrowser()
         {
             //driver.Quit();
         }
