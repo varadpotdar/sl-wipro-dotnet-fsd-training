48b68ff4c02326fcff78dddd271a36025f9e1528
varadpotdar <81146874+varadpotdar@users.noreply.github.com>
2022-07-19 15:36:07 +0530

    Phase3 - Create an MVC Application
    Create an MVC Application to Manage Data for School Application
    DESCRIPTION
    
    Using ASP.NET MVC project to create a web-based application that will allow an admin to do CRUD operations on Student, Subject, and Classe tables in the database

--- HomeController.cs
+++ HomeController.cs
@@ -1,32 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using WebApplicationMVC.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-namespace WebApplicationMVC.Controllers
+namespace Phase3proj.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public ActionResult Index()
         {
-            _logger = logger;
-        }
+            ViewBag.Title = "Home Page";
 
-        public IActionResult Index()
-        {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Phase3proj.Models
+{
+    public class StudentViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Class { get; set; }
+    }
+}
