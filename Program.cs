fa8119779ea7ab7870e6b046f4ab6932aa86f8e3
varadpotdar <81146874+varadpotdar@users.noreply.github.com>
2022-07-20 11:27:30 +0530

    Phase1- Create and Deploy Files
    Write a program that will retrieve student data from a text file and display it.

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
-using Microsoft.EntityFrameworkCore;
-using WebApplicationMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace Phase1_ReadData
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string filepath = @"C:\Users\Admin\source\repos\Phase1_ReadData\test.txt";
+
+           // string[] lines = File.ReadAllLines(filepath);
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filepath).ToList();
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            lines.Add("Varad, Div-A, 12, varad@gmail.com");
+            File.WriteAllLines(filepath,lines);
+            Console.ReadLine();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
